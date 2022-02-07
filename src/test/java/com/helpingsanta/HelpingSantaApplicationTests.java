994a2393dfd0fdf34bdb922c7edc18ee90ad15ed
Bindu <Bindu@LAPTOP-2TLJV7QC>
2022-02-07 22:38:59 +0530

    Initializing Spring boot project

---
+++ src/test/java/com/helpingsanta/HelpingSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpingsanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpingSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
